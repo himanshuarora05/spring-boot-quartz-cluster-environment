75f951758a4d8e0c28b38f0a9a1a46a3d5efbcca
Himanshu Arora <stan.himanshu@gmail.com>
2021-09-05 11:57:38 +0530

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/github/scheduler/job/SampleJob.java
@@ -0,0 +1,30 @@
+package com.github.scheduler.job;
+
+import com.github.scheduler.service.SampleService;
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@DisallowConcurrentExecution
+public class SampleJob implements Job {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SampleJob.class);
+
+    @Autowired
+    private SampleService service;
+
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) {
+        LOG.info("Starting job");
+        try {
+            service.hello();
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LOG.info("Finishing job");
+    }
+}
